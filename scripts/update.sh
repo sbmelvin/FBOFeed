6a0226a9d881acd1dd8c521b3ff09e216356828f
Stephen Melvin <stephenbmelvin@gmail.com>
2019-03-06 13:02:43 -0500

    Updated feed

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 FBO_DATA_DIR="feed"
-lftp -e "set sync-mode off; mirror -v --only-missing --include=FBOFeed* --parallel=100  -- / $FBO_DATA_DIR; quit" ftp.fbo.gov
+lftp -e "set sync-mode off; mirror -v --only-missing --include=FBOFeed* --parallel=2  -- / $FBO_DATA_DIR; quit" ftp.fbo.gov
